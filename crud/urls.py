b09b76b048536b6da6b68f9883f59dcf32a65d81
JokerSpark <92709749+JokerSpark@users.noreply.github.com>
2024-01-04 16:35:36 +0530

    Update urls.py

--- crud/urls.py
+++ crud/urls.py
@@ -19,7 +19,7 @@ from crudapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('create/',views.create,name="create"),
+    path('',views.create,name="create"),
     path('read/',views.read,name="read"),
     path('update/<int:id>',views.update,name="update"),
     path('del/<int:id>/',views.delete),
